043b2b273426d3782f4438bf465f0022ac56fd56
saniagoyal <124520922+saniagoyal@users.noreply.github.com>
2023-02-19 17:54:17 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,30 @@
+setInterval(()=>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotation=30*htime+mtime/2;
+    mrotation=6*mtime;
+    srotation=6*stime;
+    hour.style.transform=`rotate(${hrotation}deg)`
+    minute.style.transform=`rotate(${mrotation}deg)`
+    second.style.transform=`rotate(${srotation}deg)`
+},1000);
+setInterval(()=>{
+    var dateTime=new Date();
+    var hrs= dateTime.getHours();
+    var min= dateTime.getMinutes();
+    var sec= dateTime.getSeconds();
+    var session=document.getElementById('session');
+    if(hrs>=12){
+        session.innerHTML='PM';
+    }else{
+        session.innerHTML='AM';
+    }
+    if(hrs>12){
+        hrs=hrs-12;                                                                                                                        
+    }
+    document.getElementById('hours').innerHTML=hrs;
+    document.getElementById('minutes').innerHTML=min;
+    document.getElementById('seconds').innerHTML=sec;
+},1000);
